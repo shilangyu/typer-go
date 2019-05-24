6a2db1ec73731d31fd6625edf79b8718de9520b7
shilangyu <xmarcinmarcin@gmail.com>
2019-05-25 01:40:15 +0200

    added collection

--- ui/singleplayer.go
+++ ui/singleplayer.go
@@ -9,6 +9,8 @@ import (
 func CreateSingleplayer(g *gocui.Gui) error {
 	w, h := g.Size()
 
+	statsFrame := widgets.NewCollection("singleplayer-stats", "STATS", false, 0, 0, w/5, h)
+
 	stats := [...]*widgets.Text{
 		widgets.NewText("singleplayer-stats-wpm", "wpm: 0", false, false, 2, 1),
 		widgets.NewText("singleplayer-stats-time", "time: 0", false, false, 2, 2),
@@ -18,7 +20,7 @@ func CreateSingleplayer(g *gocui.Gui) error {
 
 	input := widgets.NewInput("singleplayer-input", true, false, w/5+1, h-h/6, w-w/5-1, h/6)
 
-	g.SetManager(text, input, stats[0], stats[1])
+	g.SetManager(text, input, statsFrame, stats[0], stats[1])
 
 	if err := keybindings(g); err != nil {
 		return err
