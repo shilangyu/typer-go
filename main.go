d2888e9df931dc49971614bc1b88b14cb4ac32e1
shilangyu <xmarcinmarcin@gmail.com>
2019-05-25 00:59:56 +0200

    moved keybindings to ui

--- main.go
+++ main.go
@@ -16,21 +16,7 @@ func main() {
 	err = ui.CreateWelcome(g)
 	utils.Check(err)
 
-	err = keybindings(g)
-	utils.Check(err)
-
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
-}
-
-func keybindings(g *gocui.Gui) error {
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		return err
-	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
 }
--- ui/ui.go
+++ ui/ui.go
@@ -1,6 +1,20 @@
 package ui
 
 import "github.com/shilangyu/typeracer-go/widgets"
+import	"github.com/jroimartin/gocui"
+ 
 
 // screen is a collection of current views
 var screen []widgets.Widget
+
+
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
