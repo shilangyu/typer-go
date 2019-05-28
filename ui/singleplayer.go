111402e5cb94c9a534686c56870b3d194461b0be
shilangyu <xmarcinmarcin@gmail.com>
2019-05-28 20:53:06 +0200

    more buisness logic

--- ui/singleplayer.go
+++ ui/singleplayer.go
@@ -1,30 +1,75 @@
 package ui
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/jroimartin/gocui"
 	widgets "github.com/shilangyu/gocui-widgets"
 )
 
 // CreateSingleplayer creates welcome screen widgets
 func CreateSingleplayer(g *gocui.Gui) error {
+	var currWord int
+
 	w, h := g.Size()
 
 	statsFrameWi := widgets.NewCollection("singleplayer-stats", "STATS", false, 0, 0, w/5, h)
 
-	statWis := [...]*widgets.Text{
+	statWis := []*widgets.Text{
 		widgets.NewText("singleplayer-stats-wpm", "wpm: 0", false, false, 2, 1),
 		widgets.NewText("singleplayer-stats-time", "time: 0", false, false, 2, 2),
 	}
 
-	textWi := widgets.NewText("singleplayer-text", "Cock and balls", true, false, w/5+1, 0)
+	words := strings.Split("Cock and balls", " ")
+	points := organiseText(words, 4*w/5)
+	var textWis []*widgets.Text
+	for i, p := range points {
+		textWis = append(textWis, widgets.NewText("singleplayer-text-"+strconv.Itoa(i), words[i], false, false, w/5+1+p.x, p.y))
+	}
 
-	inputWi := widgets.NewInput("singleplayer-input", true, false, w/5+1, h-h/6, w-w/5-1, h/6, nil)
+	var inputWi *widgets.Input
+	inputWi = widgets.NewInput("singleplayer-input", true, false, w/5+1, h-h/6, w-w/5-1, h/6, func(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+		if key == gocui.KeySpace || key == gocui.KeyEnter {
+			if v.Buffer() != " " {
+				currWord++
+			}
+			g.Update(inputWi.ChangeText(""))
+		} else {
+			//
+		}
+	})
 
-	g.SetManager(textWi, inputWi, statsFrameWi, statWis[0], statWis[1])
+	var wis []gocui.Manager
+	wis = append(wis, statsFrameWi)
+	for _, stat := range statWis {
+		wis = append(wis, stat)
+	}
+	for _, text := range textWis {
+		wis = append(wis, text)
+	}
+	wis = append(wis, inputWi)
+
+	g.SetManager(wis...)
 
 	if err := keybindings(g); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func organiseText(words []string, lineLength int) (points []struct{ x, y int }) {
+	x, y := 0, 0
+
+	for _, word := range words {
+		if x+len(word) > lineLength {
+			y++
+			x = 0
+		}
+		points = append(points, struct{ x, y int }{x, y})
+		x += len(word) + 1
+	}
+
+	return
 }
