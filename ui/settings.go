8d589bffb135d53d994dc31e8b3288f41b99b66b
shilangyu <xmarcinmarcin@gmail.com>
2019-06-01 22:28:05 +0200

    added sidemenu

--- ui/settings.go
+++ ui/settings.go
@@ -3,32 +3,57 @@ package ui
 import (
 	"github.com/jroimartin/gocui"
 	widgets "github.com/shilangyu/gocui-widgets"
+	"github.com/shilangyu/typer-go/settings"
 	"github.com/shilangyu/typer-go/utils"
 )
 
 // CreateSettings creates a screen with settings
 func CreateSettings(g *gocui.Gui) error {
+	var focusedView int
+
 	w, h := g.Size()
 	g.Mouse = true
 
-	infoItems := utils.Center([]string{
-		"Single player mode - test your typing skills offline!",
-		"Multi player mode - battle against other typers",
-		"Settings - change app settings",
-		"Exit - exit the app",
-	})
-	infoWi := widgets.NewText("welcome-menu-info", infoItems[0], true, true, w/2, 3*h/4)
-
 	menuItems := []string{"highlight"}
-	menuWi := widgets.NewMenu("welcome-main-menu", utils.Center(menuItems), true, true, w/2, h/2, func(i int) {
-		g.Update(infoWi.ChangeText(infoItems[i]))
-	}, nil)
+	menuWi := widgets.NewMenu("settings-menu", utils.Center(menuItems), true, true, w/4, h/2, currSetting(g), nil)
+	currSetting(g)(0)
 
-	g.SetManager(menuWi, infoWi)
+	g.SetManager(menuWi)
 
 	if err := keybindings(g, CreateWelcome); err != nil {
 		return err
 	}
+	if err := g.SetKeybinding("", gocui.KeyTab, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		focusedView++
+		_, err := g.SetCurrentView("settings-" + []string{"menu", "sidemenu"}[focusedView%2])
+		return err
+	}); err != nil {
+		return err
+	}
 
 	return nil
+}
+
+func currSetting(g *gocui.Gui) func(i int) {
+	return func(i int) {
+		w, h := g.Size()
+
+		switch i {
+		case 0:
+			menuItems := []string{
+				settings.HighlightBackground.String(),
+				settings.HighlightText.String(),
+			}
+			sideMenuWi := widgets.NewMenu("settings-sidemenu", utils.Center(menuItems), true, true, 3*w/4, h/2, func(i int) {
+				settings.I.Highlight = settings.Highlight(i)
+				settings.Save()
+			}, nil)
+			g.Update(func(g *gocui.Gui) error {
+				sideMenuWi.Layout(g)
+				sideMenuWi.ChangeSelected(int(settings.I.Highlight))(g)
+				g.SetCurrentView("settings-menu")
+				return nil
+			})
+		}
+	}
 }
