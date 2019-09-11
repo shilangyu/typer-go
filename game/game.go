03cd01300b08e6fb4df48ba7c5398b41aeac4409
shilangyu <xmarcinmarcin@gmail.com>
2019-09-11 16:24:01 +0200

    reading typer texts from path

--- game/game.go
+++ game/game.go
@@ -1,16 +1,24 @@
 package game
 
 import (
+	"errors"
 	"io/ioutil"
 	"math/rand"
+	"os"
 	"path"
+
 	"strings"
 
+	"github.com/shilangyu/typer-go/settings"
 	"github.com/shilangyu/typer-go/utils"
 )
 
 // ChooseText randomly chooses a text from the dataset
 func ChooseText() (string, error) {
+	if _, err := os.Stat(settings.I.TextsPath); os.IsNotExist(err) {
+		return "", errors.New("Didnt find typer texts, make sure your path is correct")
+	}
+
 	bytes, err := ioutil.ReadFile(path.Join(utils.Root(), "texts.txt"))
 	if err != nil {
 		return "", nil
