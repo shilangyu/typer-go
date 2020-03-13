cd3e4d3a2dbf36f1500aec8eb4d2d9ef55b847c5
shilangyu <xmarcinmarcin@gmail.com>
2020-03-13 20:21:02 +0100

    added preloaded texts closes #10

---
+++ game/default_texts.go
@@ -0,0 +1,6 @@
+package game
+
+// TyperTexts are a set of default typing texts
+const TyperTexts string = `
+im typing something
+`
--- game/game.go
+++ game/game.go
@@ -13,17 +13,20 @@ import (
 
 // ChooseText randomly chooses a text from the dataset
 func ChooseText() (string, error) {
-	if _, err := os.Stat(settings.I.TextsPath); os.IsNotExist(err) {
-		return "", errors.New("Didnt find typer texts, make sure your path is correct")
-	}
+	var texts []string
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	bytes, err := ioutil.ReadFile(settings.I.TextsPath)
-	if err != nil {
-		return "", errors.New("Couldnt load the typer texts, make sure the permission are correct")
+	if settings.I.TextsPath == "" {
+		texts = strings.Split(strings.TrimSpace(TyperTexts), "\n")
+	} else if _, err := os.Stat(settings.I.TextsPath); os.IsNotExist(err) {
+		return "", errors.New("Didn't find typer texts, make sure your path is correct or leave it empty to load some preloaded texts")
+	} else {
+		bytes, err := ioutil.ReadFile(settings.I.TextsPath)
+		if err != nil {
+			return "", errors.New("Couldnt load the typer texts, make sure the permissions are correct")
+		}
+		texts = strings.Split(strings.TrimSpace(string(bytes)), "\n")
 	}
-	texts := strings.Split(string(bytes), "\n")
-	texts = texts[:len(texts)-1]
 
 	return texts[rand.Intn(len(texts))], nil
 }
